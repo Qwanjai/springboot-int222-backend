4984c126d48d56d0770a21cb0e7a09fbbddac7a3
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2021-10-04 23:21:19 +0700

    [:package:] Add webconfig security

---
+++ src/main/java/int222/backend/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package int222.backend.config;
+
+
+//import int222.backend.security.auth.JwtAuthenticationFilter;
+//import int222.backend.security.auth.UnauthorizedEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Resource
+//    private UserDetailsService userDetailsService;
+//
+//    @Autowired
+//    private UnauthorizedEntryPoint unauthorizedEntryPoint;
+
+
+
+//    "/users/authenticate"
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.cors().and().csrf().disable();
+    }
+//        http.httpBasic().disable().authorizeRequests()  .antMatchers("/api/login", "/api/register").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint).and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+//        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+//    }
+//    @Bean
+//    public BCryptPasswordEncoder encoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    @Bean
+//    public JwtAuthenticationFilter authenticationTokenFilterBean() throws Exception {
+//        return new JwtAuthenticationFilter();
+//    }
+}
