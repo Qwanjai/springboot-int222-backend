99970737c25faccdec1b7dfc515714cd381122e2
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2021-11-03 18:37:01 +0700

    [:package:] Add Auth package

--- src/main/java/int222/backend/auth/TokenAuthenticationFilter.java
+++ src/main/java/int222/backend/auth/TokenAuthenticationFilter.java
@@ -1,13 +1,10 @@
 package int222.backend.auth;
 
-import int222.backend.services.TokenHelper;
+import int222.backend.utilities.TokenHelper;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
