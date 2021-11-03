d9db7399ba10518c35049e14bc496f2ea2dddd37
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2021-11-03 18:39:50 +0700

    [:package:] Add Service package

---
+++ src/main/java/int222/backend/models/services/ImageService.java
@@ -0,0 +1,58 @@
+package int222.backend.models.services;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Component("imgService")
+@Scope("singleton")
+public class ImageService {
+
+
+    private static final String IMAGE_PATH = "./src/images/";
+    public String saveImg(MultipartFile imageFile) throws IOException {
+//        byte[] bytes = imageFile.getBytes();
+//        Path path = Paths.get(FOLDER_URL + fileName);
+//        Files.write(path,bytes);
+//        return "Picture saved Successfully";
+        String imageName = imageFile.getOriginalFilename();
+        File myFile = new File(IMAGE_PATH + imageName);
+        myFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(myFile);
+        fos.write(imageFile.getBytes());
+        fos.close();
+        return "Upload Successfully ";
+    }
+
+
+    //    @GetMapping("/view/img/{imageName}")
+//    public ResponseEntity<byte[]> getImage(@PathVariable("imageName") String imageName) throws IOException {
+//        try {
+//            FileInputStream file = new FileInputStream(IMAGE_PATH + imageName);
+//            byte[] image = file.readAllBytes();
+//            file.close();
+//            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+//        } catch (Exception e) {
+//            throw new RemoteException("Image not Found");
+//        }
+//
+//    }
+//
+//    @GetMapping("/view/imgs")
+//    public ResponseEntity<byte[]> getAllImage() throws IOException {
+//        try {
+//            FileInputStream file = new FileInputStream(IMAGE_PATH);
+//            byte[] image = file.readAllBytes();
+//            file.close();
+//            return ResponseEntity.ok().contentType(MediaType.ALL).body(image);
+//        } catch (Exception e) {
+//            throw new RemoteException("Image not Found");
+//        }
+//
+//    }
+
+}
---
+++ src/main/java/int222/backend/models/services/MovieService.java
@@ -0,0 +1,29 @@
+package int222.backend.models.services;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import int222.backend.models.entities.Movie;
+import int222.backend.repositories.MovieRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("movieService")
+@Scope("singleton")
+@NoArgsConstructor
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepo;
+    public Movie convertJsonStringToMovie(String jsonString) throws JsonMappingException, JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Movie newMovie = mapper.readValue(jsonString, Movie.class);
+        return newMovie;
+    }
+
+    public Boolean checkNameIsAlreadyExists(String moviename){
+        return movieRepo.existsByMovienameContainsIgnoreCase(moviename);
+    }
+}
